35ce8a04634283ba4a806d9c2478aa8cc48fb67c
AlexFoster10 <wookie471@outlook.com>
2023-12-27 14:40:45 +0000

    creating player class and entity superclass and new folders

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; // 16x16 tile size
     final int scale = 3; //
 
-    final int tileSize = originalTileSize*scale; //
+    public final int tileSize = originalTileSize*scale; //
     final int maxScreenCol = 16; //
     final int maxScreenRow = 12; //
     final int screenWidth = tileSize*maxScreenCol; // 768 pixels
@@ -17,9 +19,10 @@ public class GamePanel extends JPanel implements Runnable {
     //FPS
     int FPS = 60;
 
+    //declare objects
     KeyHandler keyH = new KeyHandler();
     Thread gameThread; //repeats process again and again; clock
-
+    Player player = new Player(this,keyH);
 
     //set player defPos
     int playerX =100 , playerY = 100, playerSpeed = 4;
@@ -38,44 +41,7 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread = new Thread(this);
         gameThread.start();
     }
-//    @Override
-//    public void run() {//when we start the gameThread, it will automatically call this run method
-//
-//        double drawInterval = 1000000000/FPS; //60th of a second
-//        double nextDrawTime = System.nanoTime()+drawInterval; //preps next frame
-//        long timer = 0;
-//        int drawCount= 0;
-//        while(gameThread != null){
-//
-//            //update info i.e. player stats
-//            update();
-//            //draw screen each tick i.e. draw screen
-//            repaint();
-//
-//            try {
-//                double remainingTime = nextDrawTime-System.nanoTime(); //time till next frame
-//                timer += remainingTime;
-//                remainingTime = remainingTime/1000000; //converts nano secs to mil secs
-//
-//                if(remainingTime < 0){ //safety thing
-//                    remainingTime = 0;
-//                }
-//
-//                Thread.sleep((long) remainingTime); //sleep for time till next frame
-//
-//                nextDrawTime += drawInterval; //gets next frame time
-//                drawCount++;
-//
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            if(timer >= 1000000000){
-//                System.out.println("FPS: "+drawCount);
-//                drawCount = 0;
-//                timer = 0;
-//            }
-//        }
-//    }
+
     @Override
     public void run() {//when we start the gameThread, it will automatically call this run method
 
@@ -111,18 +77,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void update(){
 
-        if(keyH.upPressed == true){
-            playerY -= playerSpeed;
-        }
-        if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }
-        if(keyH.leftPressed == true){
-            playerX -= playerSpeed;
-        }
-        if(keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g){
@@ -131,9 +86,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D)g; //like a paintbrush with more functionality
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        player.draw(g2);
 
         g2.dispose(); //good practice to save memory
     }
