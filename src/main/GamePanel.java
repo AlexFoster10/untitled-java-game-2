97ecf404f8e44604acb669f74dcd51ed4734f3fc
AlexFoster10 <wookie471@outlook.com>
2023-12-27 04:09:43 +0000

    added to game panel class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,4 +1,25 @@
 package main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    //screen settings
+    final int originalTileSize = 16; // 16x16 tile size
+    final int scale = 3; //
+
+    final int tileSize = originalTileSize*scale; //
+    final int maxScreenCol = 16; //
+    final int maxScreenRow = 12; //
+    final int screenWidth = tileSize*maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize*maxScreenRow; // 576 pixels
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); //sets panel size
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); // does drawing offscreen
+
+    }
+
 }
