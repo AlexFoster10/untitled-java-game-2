94f62f7828d880c5365ffa7437cbba8093b7785c
AlexFoster10 <wookie471@outlook.com>
2023-12-27 12:20:10 +0000

    thread basis added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     //screen settings
     final int originalTileSize = 16; // 16x16 tile size
     final int scale = 3; //
@@ -14,12 +14,21 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize*maxScreenCol; // 768 pixels
     final int screenHeight = tileSize*maxScreenRow; // 576 pixels
 
+    Thread gameThread; //repeats process again and again; clock
 
+    //gamePanel constructor
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight)); //sets panel size
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); //does drawing offscreen
-
     }
 
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() { //when we start the gameThread, it will automatically call this run method
+
+    }
 }
