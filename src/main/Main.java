a7fed49d753334950dc38e4e35883356ee3cce80
AlexFoster10 <wookie471@outlook.com>
2023-12-27 04:12:24 +0000

    window made displayable

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,9 @@ class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Untitled Java Game");
-
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); //sets window prefs to those specified in sub comps i.e. GamePanel
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
